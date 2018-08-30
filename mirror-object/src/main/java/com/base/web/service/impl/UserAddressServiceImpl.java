b22ce64388ac955bfa45622f3e6cec5b99a8bbb2
LuoFangQing <2251234678@qq.com>
2018-08-30 13:47:20 +0800

    '地址修改'

--- mirror-object/src/main/java/com/base/web/service/impl/UserAddressServiceImpl.java
+++ mirror-object/src/main/java/com/base/web/service/impl/UserAddressServiceImpl.java
@@ -33,7 +33,6 @@ public class UserAddressServiceImpl extends AbstractServiceImpl<UserAddress, Lon
 
     @Override
     public List<Map> queryUserAddressByUserId(){
-
         Long uId = WebUtil.getLoginUser().getId();
         return getMapper().queryUserAddressByUserId(uId);
     }
